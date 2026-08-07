5d352c8f3a1b3bb6eabe34efa508bc46553ae26b
tanren <hello@tanren.dev>
2026-08-07 19:12:26 +0200

    chore: add flake devshell

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "tanren's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.prek ];
+        };
+      });
+    };
+}

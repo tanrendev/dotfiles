74b0254b9e4920879b552ad662959e3715a17916
tanren <hello@tanren.dev>
2026-08-07 19:14:40 +0200

    chore: add nixfmt

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -15,9 +15,30 @@
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in
     {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+
+      checks = forAllSystems (pkgs: {
+        nixfmt =
+          pkgs.runCommand "nixfmt"
+            {
+              nativeBuildInputs = [
+                pkgs.nixfmt
+                pkgs.findutils
+              ];
+            }
+            ''
+              cd ${self}
+              find . -name '*.nix' -exec nixfmt --check {} +
+              touch $out
+            '';
+      });
+
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
-          packages = [ pkgs.prek ];
+          packages = with pkgs; [
+            nixfmt
+            prek
+          ];
         };
       });
     };

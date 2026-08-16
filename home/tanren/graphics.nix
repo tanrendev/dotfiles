35970fa06017619d87d05883d24a8290661e3769
tanren <hello@tanren.dev>
2026-08-16 13:20:34 +0200

    fix: patch aseprite fmt include for fmt 12.2

--- home/tanren/graphics.nix
+++ home/tanren/graphics.nix
@@ -1,7 +1,15 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   home.packages = with pkgs; [
-    aseprite
+    (aseprite.overrideAttrs (
+      old:
+      lib.optionalAttrs (old.version == "1.3.18.1") {
+        postPatch = old.postPatch + ''
+          substituteInPlace src/app/i18n/strings.h \
+            --replace-fail '"fmt/core.h"' '"fmt/format.h"'
+        '';
+      }
+    ))
     fontforge-gtk
     gimp
     inkscape

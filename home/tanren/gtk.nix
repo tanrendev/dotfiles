523e82569b5c3aa12439efa6a992c4234348a6fd
tanren <hello@tanren.dev>
2026-08-16 20:17:56 +0200

    feat: live gtk3 palette switching via wrapper themes

--- home/tanren/gtk.nix
+++ home/tanren/gtk.nix
@@ -1,5 +1,22 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
+  adwWrapper =
+    variant:
+    let
+      base = "${pkgs.adw-gtk3}/share/themes/${variant}/gtk-3.0";
+      palette = "${config.xdg.configHome}/gtk-3.0/noctalia.css";
+    in
+    {
+      ".local/share/themes/${variant}/gtk-3.0/gtk.css".text = ''
+        @import url("${base}/gtk.css");
+        @import url("${palette}");
+      '';
+      ".local/share/themes/${variant}/gtk-3.0/gtk-dark.css".text = ''
+        @import url("${base}/gtk-dark.css");
+        @import url("${palette}");
+      '';
+    };
+
   papirus = pkgs.papirus-icon-theme.overrideAttrs (old: {
     postInstall = (old.postInstall or "") + ''
       for theme in Papirus-Dark Papirus-Light; do
@@ -33,8 +50,11 @@ in
 {
   home.packages = [ pkgs.adw-gtk3 ];
 
+  home.file = adwWrapper "adw-gtk3" // adwWrapper "adw-gtk3-dark";
+
   gtk = {
     enable = true;
+    gtk3.extraCss = "/* @import url(\"noctalia.css\"); */";
     iconTheme = {
       name = "Papirus-Dark";
       package = papirus;

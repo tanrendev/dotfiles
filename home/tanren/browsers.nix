58a88d7efcafdf46f66972b7e1214ac7d9b5e16e
tanren <hello@tanren.dev>
2026-08-17 23:42:10 +0200

    feat: force-install pywalfox in librewolf

--- home/tanren/browsers.nix
+++ home/tanren/browsers.nix
@@ -3,6 +3,10 @@
   programs.librewolf = {
     enable = true;
     nativeMessagingHosts = [ pkgs.pywalfox-native ];
+    policies.ExtensionSettings."pywalfox@frewacom.org" = {
+      installation_mode = "force_installed";
+      install_url = "https://addons.mozilla.org/firefox/downloads/latest/pywalfox/latest.xpi";
+    };
   };
 
   home.packages = with pkgs; [

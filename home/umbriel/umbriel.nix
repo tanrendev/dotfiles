9d0c0345a83b4c7681f92491755aa3123ebb86e8
tanren <hello@tanren.dev>
2026-08-26 12:17:56 +0200

    feat: split umbriel config and add default keybinds

--- home/umbriel/umbriel.nix
+++ home/umbriel/umbriel.nix
@@ -6,18 +6,12 @@
     enable = true;
 
     settings = {
-      workspaces.back_and_forth = true;
-
-      layout = {
-        mode = "dwindle";
-        gap = 5;
-      };
-
-      appearance = {
-        border_width = 3;
-        animation_ms = 1;
-        shadow.enabled = false;
-      };
+      include.files = [
+        "${./general.toml}"
+        "${./input.toml}"
+        "${./keybinds.toml}"
+        "${./rules.toml}"
+      ];
 
       output = {
         "HDMI-A-2".position = [
@@ -29,34 +23,6 @@
           1440
         ];
       };
-
-      input = {
-        keyboard.layout = "us";
-        touchpad = {
-          tap = true;
-          natural_scroll = true;
-        };
-        focus.follows_mouse = true;
-        cursor = {
-          theme = "grimoire-cursors";
-          size = 32;
-        };
-      };
-
-      window_rule = [
-        {
-          match.app_id = "^dev\\.noctalia\\.Noctalia$";
-          default_floating = true;
-          default_size = [
-            1080
-            920
-          ];
-        }
-        {
-          match.app_id = "thunar";
-          opacity = 0.8;
-        }
-      ];
     };
   };
 }
